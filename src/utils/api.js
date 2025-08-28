7388ef029e1230430dcc17c474f5f8f28b8e2b9b
HARIHARAN K <154298979+te5w4wzdd56@users.noreply.github.com>
2025-08-28 11:30:23 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://backend-75tr.onrender.com';
+const API_BASE_URL = 'https://backend-z630.onrender.com';
 
 // Create axios instance
 const api = axios.create({
